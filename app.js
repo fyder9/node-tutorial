a4025b2c40a8c4d5281dc725907b2e74dcab10bf
fyder9 <gossettifederico778@gmail.com>
2024-07-07 11:31:41 +0200

    cancello il primo commit

--- app.js
+++
@@ -1,8 +0,0 @@
-//manual approach (create package.json in  the root, crearte properties etc)
-//npm init (step by step)
-
-const _ = require('lodash')
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items)
-console.log(items)
-console.log(newItems)
